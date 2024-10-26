5190221fa8aa3d0faafad16213dee359dc4b9066
Dieter Limeback <dlimeb@gmail.com>
2024-10-26 12:30:38 -0400

    Disabled bufferline.nvim

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+--
+-- List of plugins installed by LazyVim to disable
+--
+
+return {
+  -- bufferline, which creates tabs for buffers
+  -- https://github.com/akinsho/bufferline.nvim
+  { "akinsho/bufferline.nvim", enabled = false },
+}
