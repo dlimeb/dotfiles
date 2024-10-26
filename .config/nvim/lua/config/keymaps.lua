9ef2ad84afc40affc4a03f4b4cdfc3329fccf89f
Dieter Limeback <dlimeb@gmail.com>
2024-10-26 16:54:43 -0400

    Update nvim keymaps with notes

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,31 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+--
 -- Most of the keymaps from my previous config are part of LazyVim's defaults,
--- or close to it. Noting them here for easy cheatsheet so I don't need to
--- visit the above URL (though there are other helpful mappings there too).
+-- or close to it.
 --
--- Window resizing is <C-up/down/left/right>
--- Clear search is <esc> (previously <leader><space> but that's for telescope now)
--- `gco` or `gcO` for add a comment above/below
--- <leader>fn to create new file
--- <leader>qq to quit all (I'll probably still :wqa or :qa!)
--- <leader>ui to inspect syntax info under cursor (previously <leader>I)
--- <leader>uw to togle line wrapping
--- `gq}` is a habit to reflow long lines, but internals have changed (cf conform.nvim) so use `gw` or gww` instead.
-
+-- The which-key plugin is great -- hit <leader> to see a menu, where the
+-- defaults are cleverly organized into categories with a memorable first
+-- character.
 --
--- CUSTOMIZATIONS
+--For now I'm going to (a) not add any key bindings and use just LazyVim
+--defaults until I have a need to customize; and (b) let which-key do its thing
+--rather than maintaining a cheatsheet.
 --
--- LazyVim's default for creating splits is <leader>- and <leader>| for
--- horiz/vert. That's a bit awkward so I'm going to try sticking with what I'm
--- familiar with for now.
--- I'm also familiar with <leader>x to close a window, but that prefix is used
--- for something else, so I'll see if I can learn the LazyVim default of
--- <leader>wc
-vim.keymap.set("n", "<Leader>v", "<C-w>v<C-w>l", {})
-vim.keymap.set("n", "<Leader>h", "<C-w>s<C-w>j", {})
--- vim.keymap.set("n", "<Leader>x", "<C-w>c", {})
-
--- Toggle showing whitespace characters
-vim.keymap.set("n", "<Leader>W", ":set nolist!<CR>", {})
