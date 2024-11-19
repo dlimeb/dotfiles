78c57116a905d651510e3c83555d1d190cb695f4
Dieter Limeback <dlimeb@gmail.com>
2024-11-18 21:31:03 -0500

    Add wezterm config file

---
+++ local/.wezterm.lua
@@ -0,0 +1,25 @@
+--
+-- Wezterm config
+-- https://wezfurlong.org/wezterm/config/files.html
+--
+
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration
+local config = wezterm.config_builder()
+
+-- Below is where you actually apply your config choices
+
+-- Change the color scheme
+config.color_scheme = "Catppuccin Macchiato"
+
+-- Set the font to use
+-- config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font_with_fallback({
+	"IBM Plex Mono",
+})
+config.font_size = 16
+
+-- Finally, return the configuration to wezterm
+return config
