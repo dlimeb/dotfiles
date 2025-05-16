68a6c5085518a464c3ef1aaf3b940844c739372d
Dieter Limeback <dlimeb@gmail.com>
2025-05-16 12:48:19 -0400

    Update nvim keymaps

--- local/.config/nvim/lua/config/keymaps.lua
+++ local/.config/nvim/lua/config/keymaps.lua
@@ -1,26 +1,38 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
 --
--- Most of the keymaps from my previous config are part of LazyVim's defaults,
--- or close to it.
+-- Keymappings are shown in a menu upon hitting <leader> by the which-key.nvim plugin
 --
--- The which-key plugin is great -- hit <leader> to see a menu, where the
--- defaults are cleverly organized into categories with a memorable first
--- character.
+-- It's possible to set keymaps here in the traditional vim way:
+-- vim.keymap.set("n", "<leader>uW", ":set list!<CR>", { desc = "Toggle whitespace" })
 --
--- For now I'm going to (a) not add any key bindings and use just LazyVim
--- defaults until I have a need to customize; and (b) let which-key do its thing
--- rather than maintaining a cheatsheet.
+-- But it's preferable to register keymaps with which-key directly, which gets
+-- some nice enhancements like icons, group setting, etc
 --
 
---- Toggle showing invisible characters
---- See vim.o.listchars in options.lua for characters to use
-vim.keymap.set("n", "<leader>uW", ":set list!<CR>", { desc = "Toggle whitespace" })
+local wk = require("which-key")
 
--- Color preview viewing, picking, and converting between formats
--- Note cursor needs to be on the color itself, not just the line
-vim.keymap.set("n", "<leader>cCt", ":CccHighlighterToggle<CR>", { desc = "Toggle color previews" })
-vim.keymap.set("n", "<leader>cCp", ":CccPick<CR>", { desc = "Open color picker" })
-vim.keymap.set("n", "<leader>cCc", ":CccConvert<CR>", { desc = "Convert color format" })
+wk.add({
+  -- Toggle showing invisible characters
+  -- See vim.o.listchars in options.lua for characters to use
+  { "<leader>uW", "<cmd>set list!<cr>", desc = "Toggle whitespace", mode = "n" },
+
+  -- Plugin: ccc.nvim
+  -- Color preview viewing, picking, and converting between formats
+  -- Note cursor needs to be on the color itself, not just the line
+  -- Register under the existing ,c group for code
+  { "<leader>cc", group = "color", desc = "Manage color" },
+  { "<leader>cct", "<cmd>CccHighlighterToggle<cr>", desc = "Toggle previews", mode = "n" },
+  { "<leader>ccp", "<cmd>CccP<cr>", desc = "Open picker", mode = "n" },
+  { "<leader>ccc", "<cmd>CccConvert<cr>", desc = "Convert format", mode = "n" },
+
+  -- Plugin: obsidian.nvim
+  -- See https://github.com/epwalsh/obsidian.nvim?tab=readme-ov-file#commands for all available commands
+  { "<leader>o", group = "obsidian", desc = "Obsidian" },
+
+  { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "New note", mode = "n" },
+  { "<leader>oo", "<cmd>ObsidianQuickSwitch<cr>", desc = "Open note", mode = "n" },
+  -- { "<leader>od", "<cmd>ObsidianToday<cr>", desc = "Open today", mode = "n" },
+  { "<leader>or", "<cmd>ObsidianRename<cr>", desc = "Rename note", mode = "n" },
+  { "<leader>ot", "<cmd>ObsidianTemplate<cr>", desc = "Insert template", mode = "n" },
+})
--- local/.config/nvim/lua/plugins/obsidian.lua
+++ local/.config/nvim/lua/plugins/obsidian.lua
@@ -1,3 +1,7 @@
+---
+--- https://github.com/epwalsh/obsidian.nvim
+---
+
 return {
   "epwalsh/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
@@ -27,14 +31,99 @@ return {
       -- },
     },
 
-    -- Whether obsidian.nvim should manage frontmatter automatically
+    -- Main notes directory, if it is a specific subdirectory of the vault
+    notes_subdir = "Notes",
+    new_notes_location = "notes_subdir",
+
+    -- Templates directory and config
+    templates = {
+      folder = "Utilities/Templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+    },
+
+    -- Daily notes
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      -- TODO: This is not handling my yyyy/mm/dd subdirectories
+      -- :ObsidianToday will create a new note in /Calendar only -- the daily
+      -- note template *does* move new files, but presumably opening will still
+      -- not work
+      folder = "Calendar",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      -- date_format = "%Y-%m-%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      -- alias_format = "%B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      -- default_tags = { "daily-notes" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      -- template = nil,
+    },
+
+    -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+    -- TODO: Probably want to enable this at some point
+    completion = {
+      -- Set to false to disable completion.
+      -- nvim_cmp = true,
+      -- Trigger completion at 2 chars.
+      -- min_chars = 2,
+    },
+
+    -- Optional, customize how note IDs are generated given an optional title.
+    --
+    -- Based on docs example of creating note filenames in a zettelkasten
+    -- format with an ID, which I don't need. With a title like "My new note"
+    -- provided, this will create a filename of "my-new-note.md" (note this is
+    -- different than the Obsidian desktop app, where a template auto-adds a
+    -- title based on the filename, so I have a lot of "My new note.md") If a
+    -- title is not provided, this creates a filename like "Untitled-XXXX.md"
+    -- with 4 random letters.
+    -- WARN: A randomized suffix is a good idea overall, bc this will happily
+    -- overwrite an existing file if I choose a title that already exists. If I
+    -- end up doing obsidian things in nvim a lot I should handle this
+    -- properly.
+    --
+    note_id_func = function(title)
+      local filename = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        filename = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters
+        filename = "Untitled-"
+        for _ = 1, 4 do
+          filename = filename .. string.char(math.random(65, 90))
+        end
+      end
+      return filename
+    end,
+
+    -- Frontmatter
+    -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
     disable_frontmatter = true,
 
+    -- Configure syntax highlighting
+    -- I am disabling this for now, since it requires `conceallevel` set to 1 or
+    -- 2. I have it at 0 since I want to see straight markdown in vim, at least
+    --    for now.
+    --
+    -- See the obsidian.nvim docs for additional settings if I end up enabling
+    -- this -- you can control checkbox display, which syntax highlight groups
+    -- get assigned, etc.
     ui = {
-      -- Disable fancy syntax highlighting since it requires `conceallevel` set
-      -- to 1 or 2 -- I have it at 0 since I want straight markdown in vim, at
-      -- least for now
       enable = false,
+    },
+
+    -- Keymappings
+    -- Disable here and set in .config/nvim/lua/config/keymaps.lua for
+    -- convenience bc likely to just need to map key combo to existing command.
+    -- But see obsidian.nvim docs; can define keymaps that are functions here
+    -- if needed
+    mappings = {},
+
+    -- Attachments
+    attachments = {
+      img_folder = "Utilities/Attachments",
     },
   },
 }
