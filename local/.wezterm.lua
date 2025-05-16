19ef85ff2db72092bda46ce428fa8efa2645ea84
Dieter Limeback <dlimeb@gmail.com>
2025-05-16 12:48:29 -0400

    Change terminal font

--- local/.wezterm.lua
+++ local/.wezterm.lua
@@ -16,9 +16,13 @@ config.color_scheme = "catppuccin-mocha"
 
 -- Set the font to use
 config.font = wezterm.font_with_fallback({
-	"IBM Plex Mono",
+	-- "IBM Plex Mono",
+	"Geist Mono",
 })
 config.font_size = 16
+
+-- Disable font ligatures
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 -- Finally, return the configuration to wezterm
 return config
